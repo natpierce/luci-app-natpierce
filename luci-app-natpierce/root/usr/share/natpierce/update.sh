93b3f3a03537b28724f791d498199bed1377ab8d
xiyu505 <3555149315@qq.com>
2025-08-21 00:53:54 +0800

    增加防御性代码

--- luci-app-natpierce/root/usr/share/natpierce/update.sh
+++ luci-app-natpierce/root/usr/share/natpierce/update.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 uci_tool=/sbin/uci
-
+PROG_BIN="/usr/share/natpierce/natpierce"
 # 官网版本号 URL
 url="https://www.natpierce.cn/tempdir/info/version.html"
-
 latest_version=$(wget -qO- "$url")
+
+if [ ! -f "$PROG_BIN" ]; then
+    $uci_tool set natpierce.status.current_version="N/A"
+    $uci_tool commit natpierce
+fi
 
 if [ -n "$latest_version" ]; then
     $uci_tool set natpierce.status.latest_version="$latest_version"
@@ -13,6 +17,8 @@ if [ -n "$latest_version" ]; then
     echo "获取最新版本号成功：$latest_version"
     exit 0
 else
+    $uci_tool set natpierce.status.latest_version="N/A"
+    $uci_tool commit natpierce
     echo "错误：无法获取最新版本号，请检查网络。"
     exit 1
 fi
